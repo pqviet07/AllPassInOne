8d3266169c6d1f4a5a62f2192cc7f147f242caaa
Phung Quoc Viet <phungquocviet07@gmail.com>
2021-07-20 16:12:08 -0700

    first commit

---
+++ PasswordManager/PasswordManager/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PasswordManager
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_fullname = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_fullname
+            // 
+            this.lb_fullname.AutoSize = true;
+            this.lb_fullname.Location = new System.Drawing.Point(131, 140);
+            this.lb_fullname.Name = "lb_fullname";
+            this.lb_fullname.Size = new System.Drawing.Size(35, 13);
+            this.lb_fullname.TabIndex = 0;
+            this.lb_fullname.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_fullname);
+            this.Name = "MyForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_fullname;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AllPassInOne
+# AllPassInOn
+test
